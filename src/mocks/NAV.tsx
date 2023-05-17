49cae8afb5a2dddcec222f54975702f611b2f708
zinukk <slsxpseh54@naver.com>
2023-05-18 05:41:08 +0900

    ADD : Home Header 추가

--- src/mocks/NAV.tsx
+++ src/mocks/NAV.tsx
@@ -7,28 +7,28 @@ import { IoStorefrontOutline, IoStorefrontSharp } from 'react-icons/io5';
 export const NAV: Nav[] = [
   {
     id: 0,
-    name: 'Home',
+    name: '홈',
     path: '/',
     active: <AiFillHome size={25} color="#5655a5" />,
     disabled: <AiOutlineHome size={25} />,
   },
   {
     id: 1,
-    name: 'Store',
+    name: '매장',
     path: '/store',
     active: <IoStorefrontSharp size={25} color="#5655a5" />,
     disabled: <IoStorefrontOutline size={25} />,
   },
   {
     id: 2,
-    name: 'Robot',
+    name: '로봇',
     path: '/robot',
     active: <AiFillRobot size={25} color="#5655a5" />,
     disabled: <AiOutlineRobot size={25} />,
   },
   {
     id: 3,
-    name: 'Error',
+    name: '에러',
     path: '/error',
     active: <MdError size={25} color="#5655a5" />,
     disabled: <MdErrorOutline size={25} />,
