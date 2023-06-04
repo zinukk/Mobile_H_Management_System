57105cd7944b4d034c2313726d6e8ef2f78c8e3d
zinukk <slsxpseh54@naver.com>
2023-06-05 00:41:00 +0900

    ADD : RobotCard 컴포넌트 구현 완료

---
+++ src/types/robot.ts
@@ -0,0 +1,32 @@
+export type IRobotState = {
+  battery: string;
+  current_node: string;
+  distance: string;
+  error_type: string;
+  final_node: string;
+  k_map_name: string;
+  map_id: string;
+  map_name: string;
+  max_voltage: string;
+  min_voltage: string;
+  robot_home: string;
+  robot_id: string;
+  robot_state: string;
+  serial_number: string;
+  serving_count: string;
+  start_at: string;
+  state: string;
+  table_id: string;
+  target_node: string;
+  updated_at: string;
+};
+
+export type IRobotType = {
+  id: string;
+  state: string;
+  color: string;
+};
+
+export type IStoreNameObj = {
+  [index: string]: string;
+};
