ce54b00bbc6e6408316f5706f884102beac462d0
zinukk <slsxpseh54@naver.com>
2023-06-15 20:27:05 +0900

    ADD : Null 컴포넌트 구현

---
+++ src/components/Common/Null.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+const Null = () => {
+  return <StNull>No Result</StNull>;
+};
+
+const StNull = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: ${({ theme }) => theme.color.gray600};
+  font-size: 20px;
+`;
+
+export default Null;
