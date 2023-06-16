6e445a810b3ebc6b3cb35920c5209a17e29347a7
zinukk <slsxpseh54@naver.com>
2023-06-16 17:16:51 +0900

    Modify : Calendar 컴포넌트 변경

--- src/components/Common/Calendar.tsx
+++ src/components/Common/Calendar.tsx
@@ -1,80 +1,41 @@
-import styled from '@emotion/styled';
+import React, { Dispatch, SetStateAction } from 'react';
 import DatePicker from 'react-datepicker';
 import ko from 'date-fns/locale/ko';
 import 'react-datepicker/dist/react-datepicker.css';
-import React, { Dispatch, SetStateAction } from 'react';
-import { IDates } from '@src/types/error';
+import styled from '@emotion/styled';
 
 interface IProps {
-  event: (arg: IDates) => void;
-  storeId: number;
-  startDate: Date;
-  endDate: Date;
-  setStartDate: Dispatch<SetStateAction<Date>>;
-  setEndDate: Dispatch<SetStateAction<Date>>;
+  date: Date;
+  setDate: Dispatch<SetStateAction<Date>>;
 }
 
-const Calendar = ({ event, storeId, startDate, setStartDate, endDate, setEndDate }: IProps) => {
-  const startDateHandler = (date: Date) => {
-    setStartDate(date);
-  };
-
-  const endDateHandler = (date: Date) => {
-    setEndDate(date);
-  };
-
-  const convertDate = (date: Date) => {
-    const year = date.getFullYear().toString();
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const day = ('0' + date.getDate()).slice(-2);
-
-    return year + '-' + month + '-' + day;
-  };
-
-  const data = {
-    start_date: convertDate(startDate),
-    end_date: convertDate(endDate),
-    map_id: storeId,
+const Calendar = ({ date, setDate }: IProps) => {
+  const dateHandler = (date: Date) => {
+    setDate(date);
   };
 
   return (
-    <StCalendar>
-      <StCustomDatePicker
+    <StContainer>
+      <StCalendar
         locale={ko}
         dateFormat="yyyy.MM.dd (eee)"
         showPopperArrow={false}
-        selected={startDate}
+        selected={date}
         onChange={(date: Date) => {
-          startDateHandler(date);
+          dateHandler(date);
         }}
       />
-      <StCustomDatePicker
-        locale={ko}
-        dateFormat="yyyy.MM.dd (eee)"
-        showPopperArrow={false}
-        minDate={startDate}
-        selected={endDate}
-        onChange={(date: Date) => {
-          endDateHandler(date);
-        }}
-      />
-      <StSubmitBtn
-        onClick={() => {
-          event(data);
-        }}>
-        검색
-      </StSubmitBtn>
-    </StCalendar>
+    </StContainer>
   );
 };
 
-const StCalendar = styled.div`
-  margin-top: 10px;
-  display: flex;
-  width: 100%;
+const StContainer = styled.div`
+  .react-datepicker__input-container {
+    width: 120px;
+  }
 `;
 
-const StCustomDatePicker = styled(DatePicker)`
+const StCalendar = styled(DatePicker)`
   padding: 0 5px;
   width: 120px;
   height: 30px;
@@ -89,18 +50,6 @@ const StCustomDatePicker = styled(DatePicker)`
   :hover {
     background: ${({ theme }) => theme.color.main};
   }
-`;
-
-const StSubmitBtn = styled.button`
-  padding: 0 5px;
-  width: 120px;
-  height: 30px;
-  color: ${({ theme }) => theme.color.white};
-  background: ${({ theme }) => theme.color.main};
-  border-radius: 5px;
-  font-size: 11px;
-  border: none;
-  cursor: pointer;
 `;
 
 export default Calendar;
