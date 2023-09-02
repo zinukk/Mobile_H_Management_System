5a73588ef552a3ef587796545ec8ec46ee185f10
zinukk <slsxpseh54@naver.com>
2023-09-02 22:31:58 +0900

    Modify : axios 설정 변경

--- src/api/client.ts
+++ src/api/client.ts
@@ -6,7 +6,6 @@ export const baseURL: string = process.env.NEXT_PUBLIC_BASE_URL as string;
 
 const client = axios.create({
   baseURL: `${baseURL}`,
-  timeout: 3000,
   headers: { 'Content-Type': 'application/json;charset=utf-8' },
 });
 
